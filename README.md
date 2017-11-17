a7c909168d530dea7a2fb5afc9e119d40a003bda
Mårten Söderberg <marten@martensoderberg.se>
2017-11-17 18:17:50 +0100

    initial progress -- reading a file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 Conway's Game of Life
 Implemented in Python
+
+Input args to gol.py:
+1. A filename
+
+Each character in the input file (given by argument 1) is parsed as a cell in the game world. '1' is parsed as a living cell, newline characters are parsed as new lines in the world (which is created from the top-left corner), every other character is parsed as a dead cell.
+
+The input world is assumed to be rectangular. In particular, all lines of the input file should be of equal length. Otherwise, gol.py will return an error.
---
+++ gol.py
@@ -0,0 +1,74 @@
+import sys
+
+usage_text = "usage: gol.py <input file> [iteration count]"
+
+# Parses the program arguments and either exits with an error if something went
+# wrong, or returns a (file, count) tuple, where:
+#  - file    is the input file describing the world
+#  - count   is the number of iterations to advance the world
+#
+# Note that if no iteration count arguemnt is given, it is assumed to be 1
+def parse_args():
+  # sys.argv[0] will always be "gol.py"
+  argv = sys.argv
+  argc = len(argv)
+  if argc < 2:
+    # TODO: error class?
+    raise ValueError("Too few arguments!")
+  elif argc == 2:
+    iteration_count = 1
+  else:
+    try:
+      iteration_count = int(argv[2])
+    except ValueError:
+      # TODO: error class?
+      raise ValueError("Could not parse iteration count: " + argv[2])
+
+  filepath = argv[1]
+
+  try:
+    input_file = open(filepath, "r")
+  except FileNotFoundError:
+    raise FileNotFoundError("Could not open filepath: " + filepath)
+
+  return (input_file, iteration_count)
+
+# build_world takes a file as its input and outputs its world representation
+def build_world(input_file):
+  lines = 0
+  line_len = 0
+  for line in input_file:
+    if line_len is not 0 and len(line) is not len_line:
+      # TODO: error class??
+      raise ValueError("Varying-length lines in input file!")
+    print(line.strip())
+    lines += 1
+
+  return None
+
+
+######################
+# MAIN PROGRAM START #
+######################
+try:
+  (input_file, iteration_count) = parse_args()
+except Exception as error:
+  print(error)
+  print(usage_text)
+  exit(1)
+
+try:
+  world = build_world(input_file)
+except Exception as error:
+  print(error)
+  print(usage_text)
+  input_file.close()
+  exit(2)
+
+input_file.close()
+
+
+
+
+
+
